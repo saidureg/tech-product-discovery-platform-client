b515b7f5cd72f2b29c3ae7075c5dfda322519e46
saidureg <saidureg@gmail.com>
2023-11-27 18:52:53 +0600

    user can search by tags name

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,4 +1,4 @@
-// import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Helmet } from "react-helmet-async";
 import ProductCard from "./ProductCard";
 import useAxiosPublic from "../../hooks/useAxiosPublic";
@@ -6,22 +6,49 @@ import { useQuery } from "@tanstack/react-query";
 
 const Product = () => {
   const axiosPublic = useAxiosPublic();
+  const [search, setSearch] = useState("");
+  // const [product, setProduct] = useState([]);
 
-  const { data: products = [] } = useQuery({
-    queryKey: ["product"],
+  const { data: product = [] } = useQuery({
+    queryKey: ["product", search ? search : ""],
     queryFn: async () => {
-      const res = await axiosPublic.get("/product");
+      const res = await axiosPublic.get(`/product/search?search=${search}`);
       return res.data;
     },
   });
+
+  // useEffect(() => {
+  //   axiosPublic
+  //     .get(`/product/search?search=${search}`)
+  //     .then((res) => {
+  //       setProduct(res.data);
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // }, [axiosPublic, search]);
 
   const handleSearch = (e) => {
     e.preventDefault();
     const form = e.target;
     const searchValue = form.search.value;
-    console.log(searchValue);
+    setSearch(searchValue);
     form.reset();
   };
+
+  // const sortedData = data.sort((a, b) => {
+  //   const timeA = new Date(a.time);
+  //   const timeB = new Date(b.time);
+
+  //   // Compare dates first
+  //   if (timeA.toDateString() !== timeB.toDateString()) {
+  //     return timeB - timeA;
+  //   }
+
+  //   // If the dates are the same, compare times
+  //   return timeB.getTime() - timeA.getTime();
+  // });
+
   return (
     <div>
       <Helmet>
@@ -34,7 +61,7 @@ const Product = () => {
         <input
           name="search"
           type="text"
-          placeholder="Search here...."
+          placeholder="Search by tags...."
           className="input input-bordered w-11/12 md:w-full max-w-md text-[#0B0B0B66]"
         />
         <button className="py-3 px-5 bg-[#FF444A] rounded-r-lg text-white absolute right-4 md:right-[161px] lg:right-[416px]">
@@ -42,7 +69,7 @@ const Product = () => {
         </button>
       </form>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 my-10 mx-4 lg:mx-0">
-        {products?.map((product, idx) => (
+        {product?.map((product, idx) => (
           <ProductCard key={idx} product={product} />
         ))}
       </div>
