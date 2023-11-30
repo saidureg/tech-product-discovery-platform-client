f26739fb161b58ef6d9dd4e664f346eec1ac478a
saidureg <saidureg@gmail.com>
2023-11-30 18:58:53 +0600

    show Membership status verify for Subscription user

--- src/pages/Dashboard/User/Profile/UserProfile.jsx
+++ src/pages/Dashboard/User/Profile/UserProfile.jsx
@@ -1,9 +1,20 @@
 import { Helmet } from "react-helmet-async";
 import useAuth from "../../../../hooks/useAuth";
 import { Link } from "react-router-dom";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
 
 const UserProfile = () => {
   const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data: payments = [] } = useQuery({
+    queryKey: ["payments", user?.email],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/payments/${user?.email}`);
+      return res.data;
+    },
+  });
+
   const paymentAmount = 1000;
   const handlePayment = (e) => {
     e.preventDefault();
@@ -31,13 +42,15 @@ const UserProfile = () => {
               className="mx-auto object-cover rounded-full h-24 w-24  border-2 border-white "
             />
           </a>
+          {payments.length > 0 ? (
+            <p className="p-2 px-4 text-xs text-white bg-pink-500 rounded-full">
+              {" "}
+              Subscription Member
+            </p>
+          ) : (
+            ""
+          )}
 
-          <p className="p-2 px-4 text-xs text-white bg-pink-500 rounded-full">
-            {/* {role && role.toUpperCase()} */} Member
-          </p>
-          <p className="mt-2 text-xl font-medium text-gray-800 ">
-            {/* User Id: {user.uid} */} User Id
-          </p>
           <div className="w-full p-2 mt-4 rounded-lg">
             <div className="flex flex-col lg:flex-row flex-wrap items-center justify-between text-sm text-gray-600 ">
               <p className="flex flex-col">
@@ -51,33 +64,39 @@ const UserProfile = () => {
                 <span className="font-bold text-black ">{user?.email}</span>
               </p>
 
-              <div>
-                <form
-                  onSubmit={handlePayment}
-                  className="relative flex justify-center items-center my-10"
-                >
-                  <input
-                    name="coupon"
-                    type="text"
-                    placeholder="Coupon Code"
-                    className="input input-bordered w-11/12 md:w-full max-w-md text-[#0B0B0B66]"
-                  />
-                  <button className="py-3 px-5 bg-[#FF444A] rounded-r-lg text-white  absolute right-[10px] md:right-[2px] lg:right-0">
-                    Apply
-                  </button>
-                </form>
-                <p>
-                  Status:
-                  <span className="p-1 px-2 text-xs text-white bg-[#af4053] rounded-lg">
-                    Verified
-                  </span>
-                </p>
-                <Link to="/dashboard/payment">
-                  <button className="bg-[#F43F5E] px-10 py-1 rounded-lg text-white cursor-pointer hover:bg-[#af4053] block my-1">
-                    Subscribe <span>{paymentAmount}</span>
-                  </button>
-                </Link>
-              </div>
+              {payments.length > 0 ? (
+                <div>
+                  <p>
+                    Membership Status:
+                    <span className="p-1 px-2 text-xs text-white bg-[#af4053] rounded-lg mr-5">
+                      {/*  */}
+                      Verified
+                    </span>
+                  </p>
+                </div>
+              ) : (
+                <div>
+                  <form
+                    onSubmit={handlePayment}
+                    className="relative flex justify-center items-center my-4"
+                  >
+                    <input
+                      name="coupon"
+                      type="text"
+                      placeholder="Coupon Code"
+                      className="input input-bordered w-11/12 md:w-full max-w-md text-[#0B0B0B66]"
+                    />
+                    <button className="py-3 px-5 bg-[#FF444A] rounded-r-lg text-white  absolute right-[10px] md:right-[2px] lg:right-[3px]">
+                      Apply
+                    </button>
+                  </form>
+                  <Link to="/dashboard/payment">
+                    <button className="bg-[#F43F5E] px-10 py-2 rounded-lg text-white cursor-pointer hover:bg-[#af4053] block">
+                      Subscribe <span>{paymentAmount}</span>
+                    </button>
+                  </Link>
+                </div>
+              )}
             </div>
           </div>
         </div>
