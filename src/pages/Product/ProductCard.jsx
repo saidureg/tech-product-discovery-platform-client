5e3000880d1f9be1ef5761b06346a8cf92a10303
saidureg <saidureg@gmail.com>
2023-11-30 23:29:27 +0600

    updated framer motion styles for product card

--- src/pages/Product/ProductCard.jsx
+++ src/pages/Product/ProductCard.jsx
@@ -32,13 +32,22 @@ const ProductCard = ({ product }) => {
   return (
     <div className="card card-compact bg-base-100 hover:bg-base-200 shadow-xl">
       <motion.div
+        initial={{ scale: 0 }}
+        animate={{ rotate: 360, scale: 1 }}
+        transition={{
+          type: "spring",
+          stiffness: 260,
+          damping: 20,
+        }}
+      >
+        {/* <motion.div
         whileHover={{ scale: 1.1, rotate: 360 }}
         whileTap={{
           scale: 0.7,
           rotate: -90,
           borderRadius: "100%",
         }}
-      >
+      > */}
         <figure>
           <img
             className="w-full h-[200px] rounded-lg"
