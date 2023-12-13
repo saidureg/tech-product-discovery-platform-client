91013a420cca4a986d7523d40a144fa0af4cd2a6
saidureg <saidureg@gmail.com>
2023-12-13 19:41:28 +0600

    fixed some problem

--- src/components/Shared/Vote/DownVote.jsx
+++ src/components/Shared/Vote/DownVote.jsx
@@ -24,7 +24,6 @@ const DownVote = ({ dVote_count, refetch, id }) => {
     }
     const findDownVotes = downVote?.find((item) => item.product_id === id);
     setFindDownVotes(findDownVotes);
-    console.log(findDownVotes);
   }, [products, id, user, downVote]);
 
   const handleDownVote = async () => {
